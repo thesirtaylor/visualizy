3b1b1d387cf086119cf41356f95d481f9c5902b5
Ajayi Olatunji O <ajayiolatunji17@gmail.com>
2024-04-14 13:20:56 +0100

    initiate database microservice

--- apps/database-service/src/main.ts
+++ apps/database-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { DatabaseServiceModule } from './database-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(DatabaseServiceModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    DatabaseServiceModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: [''],
+        },
+        consumer: {
+          groupId: 'database-consumer',
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
